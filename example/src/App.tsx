3a7f723c2eb43c18ed57d0d1bd9a72debbdb5cd8
KagnSeohyeon <kshyeon123@gmail.com>
2023-07-10 18:18:15 +0900

    connecting component

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import { ThemeProvider, createGlobalStyle } from "styled-components";
+import { Pagination } from "../../src/index";
 
 const GlobalStyling = createGlobalStyle`
   * {
@@ -11,11 +12,17 @@ const GlobalStyling = createGlobalStyle`
 `;
 
 const App = () => {
+  const [curPage, setCurPage] = useState<number>(1);
+  const onClick = (e: any) => {
+    console.log(e);
+    setCurPage(curPage);
+  };
+
   return (
     <React.StrictMode>
       <ThemeProvider theme={{}}>
         <GlobalStyling />
-        <div>Application</div>
+        <Pagination curPage={curPage} totalPage={3} onClick={onClick} />
       </ThemeProvider>
     </React.StrictMode>
   );
